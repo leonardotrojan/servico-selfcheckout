143d36b297806bceb8daf2a9618a5eb348657e6e
Leonardo Trojan <leo.troyan.melo@gmail.com>
2026-01-19 18:41:43 -0300

    completando a pagina home e iniciando o menu

---
+++ self-checkout/src/app/[slug]/menu/page.tsx
@@ -0,0 +1,34 @@
+import { db } from "@/lib/prisma";
+import { notFound } from "next/navigation";
+import Image from "next/image"
+import { Button } from "@/components/ui/button";
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import RestaurantHeader from "./components/header";
+
+interface RestaurantMenuPageProps {
+    params: Promise<{slug: string}>
+    searchParams: Promise<{consumptionMethod: string}>
+}
+
+const isConsumptionMethodValid = (ConsumptionMethod: string) => {
+    return ["DINE_IN", "TAKEAWAY"].includes(ConsumptionMethod.toUpperCase())
+}
+
+const RestaurantMenuPage = async ({params, searchParams}: RestaurantMenuPageProps) => {
+    const {slug} = await params
+    const {consumptionMethod} = await searchParams
+    if (!isConsumptionMethodValid(consumptionMethod)) {
+        return notFound()
+    }
+    const restaurant = await db.restaurant.findUnique({ where: {slug: slug}})
+    if (!restaurant) {
+        return notFound()
+    }
+    return ( 
+        <div>
+            <RestaurantHeader restaurant={restaurant}/>
+        </div>
+     );
+}
+ 
+export default RestaurantMenuPage;
---
+++ self-checkout/src/app/[slug]/page.tsx
@@ -0,0 +1,57 @@
+import { db } from "@/lib/prisma"
+import Image from "next/image"
+import { notFound } from "next/navigation"
+import ConsumptionMethodOption from "./components/Consumption-method-option"
+
+interface RestaurantPageProps {
+    params: Promise<{ slug: string }>
+}
+
+const RestaurantPage = async ({ params }: RestaurantPageProps) => {
+    const { slug } = await params
+    const restaurant = await db.restaurant.findUnique({ where: {slug: slug} });
+    if (!restaurant) {
+        return notFound()
+    }
+    return (
+        <div className="h-screen flex flex-col items-center justify-center px-6 pt-24">
+            {/* Logo e titulo */}
+            <div className="flex flex-col items-center gap-2">
+                <Image 
+                  src={restaurant.avatarImageUrl} 
+                  alt={restaurant?.name} 
+                  width={82} 
+                  height={82}
+                />
+                <h2 className="font-semibold">{restaurant.name}</h2>
+            </div>
+            {/* bem vindo */}
+            <div className="pt-24 text-center space-y-2">
+                <h3 className="text-2xl font-semibold">
+                    Seja bem-vindo!
+                </h3>
+                <p className="opacity-55">
+                    Escolha como prefere aproveitar sua refeição. Estamos oferecer particularidade e sabor em cada detalhe
+                </p>
+            </div>
+            <div className="pt-14 grid grid-cols-2 gap-4">
+                <ConsumptionMethodOption 
+                    slug={slug}
+                    buttonText="Para comer aqui"
+                    imageAlt="Comer aqui"
+                    imageUrl="/dine_in.png"
+                    option="DINE_IN"
+                />
+                <ConsumptionMethodOption 
+                    slug={slug}
+                    buttonText="Para levar"
+                    imageAlt="Para levar"
+                    imageUrl="/takeaway.png"
+                    option="TAKEAWAY"
+                />
+            </div>
+        </div>
+    )
+}
+ 
+export default RestaurantPage;
