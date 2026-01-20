2687ac5e9cde0532f785002a107d91ea5f888aad
Leonardo Trojan <leo.troyan.melo@gmail.com>
2026-01-20 01:24:16 -0300

    componente header do produto

---
+++ self-checkout/src/app/[slug]/menu/[productId]/page.tsx
@@ -0,0 +1,24 @@
+import { db } from "@/lib/prisma";
+import { notFound } from "next/navigation";
+import ProductHeader from "./components/Product-header";
+
+
+interface ProductPageProps {
+    params: Promise<{ slug: string; productId: string }>
+}
+
+const ProductPage = async ({params}: ProductPageProps) => {
+    const {slug, productId} = await params;
+    const product = await db.product.findUnique({ where: { id: productId } })
+    if (!product) {
+        return notFound()
+    }
+   
+    return ( 
+        <>
+          <ProductHeader product={product} />
+        </>
+     );
+}
+ 
+export default ProductPage;
