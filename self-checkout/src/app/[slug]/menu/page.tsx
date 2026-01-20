ef1b9badbac62e83f1e3c7aff54f24b3741e89a7
Leonardo Trojan <leo.troyan.melo@gmail.com>
2026-01-19 23:21:59 -0300

    add pagina do menu do restaurante

--- self-checkout/src/app/[slug]/menu/page.tsx
+++ self-checkout/src/app/[slug]/menu/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import { Button } from "@/components/ui/button";
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import RestaurantHeader from "./components/header";
+import RestaurantCategories from "./components/categories";
 
 interface RestaurantMenuPageProps {
     params: Promise<{slug: string}>
@@ -20,13 +21,25 @@ const RestaurantMenuPage = async ({params, searchParams}: RestaurantMenuPageProp
     if (!isConsumptionMethodValid(consumptionMethod)) {
         return notFound()
     }
-    const restaurant = await db.restaurant.findUnique({ where: {slug: slug}})
+    const restaurant = await db.restaurant.findUnique({ 
+        where: {
+            slug: slug
+        }, 
+        include: {
+            menuCategories: {
+                include: {
+                    products: true
+                }
+            }
+        }})
+
     if (!restaurant) {
         return notFound()
     }
     return ( 
         <div>
             <RestaurantHeader restaurant={restaurant}/>
+            <RestaurantCategories restaurant={restaurant} />
         </div>
      );
 }
