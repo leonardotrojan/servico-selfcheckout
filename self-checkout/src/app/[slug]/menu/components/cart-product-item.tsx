661021b91082304f4e50755286ac872afa4dd27d
Leonardo Trojan <leo.troyan.melo@gmail.com>
2026-01-24 14:44:56 -0300

    quantidade do produto pode ser diminuida

--- self-checkout/src/app/[slug]/menu/components/cart-product-item.tsx
+++ self-checkout/src/app/[slug]/menu/components/cart-product-item.tsx
@@ -1,14 +1,16 @@
 import { formatCurrency } from "@/helpers/format-currency";
-import { CartProduct } from "../contexts/cart";
+import { CartContext, CartProduct } from "../contexts/cart";
 import Image from "next/image"
 import { ChevronLeftIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { useContext } from "react";
 
 interface CartProductProps {
     product: CartProduct;
 }
 
 const CartProductItem = ({product}: CartProductProps) => {
+  const {decreaseProductQuantity} = useContext(CartContext)
     return ( 
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-3">
@@ -26,6 +28,7 @@ const CartProductItem = ({product}: CartProductProps) => {
               <Button
                  variant="outline"
                  className="w-7 h-7 rounded-lg"
+                 onClick={() => decreaseProductQuantity(product.id)}
               >
                 <ChevronLeftIcon />
               </Button>
