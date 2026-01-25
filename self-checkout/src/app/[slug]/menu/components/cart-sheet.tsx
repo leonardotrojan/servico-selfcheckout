fbe44b8ca7d343074f33897ac6fa774e59c18801
Leonardo Trojan <leo.troyan.melo@gmail.com>
2026-01-25 14:37:16 -0300

    add componente inicial que finaliza o pedido

--- self-checkout/src/app/[slug]/menu/components/cart-sheet.tsx
+++ self-checkout/src/app/[slug]/menu/components/cart-sheet.tsx
@@ -1,12 +1,15 @@
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "../contexts/cart";
 import CartProductItem from "./cart-product-item";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { formatCurrency } from "@/helpers/format-currency";
+import FinishOrderButton from "./finish-order-dialog";
+import FinishOrderDialog from "./finish-order-dialog";
 
 const CartSheet = () => {
+    const [finishOrderDialogIsOpen, setFinishOrderDialogIsOpen] = useState(false)
     const {isOpen, toggleCart, products, total} = useContext(CartContext)
 
     return ( 
@@ -29,9 +32,13 @@ const CartSheet = () => {
                             </div>
                         </CardContent>
                     </Card>
-                    <Button className="w-full rounded-full">
+                    <Button
+                     className="w-full rounded-full"
+                     onClick={() => setFinishOrderDialogIsOpen(true)}
+                    >
                         Finalizar pedido
                     </Button>
+                    <FinishOrderDialog open={finishOrderDialogIsOpen} onOpenChange={setFinishOrderDialogIsOpen} />
                 </div>
             </SheetContent>
         </Sheet>
