e021416dced640f6c788a77f06de60a5ef4e9d7d
Leonardo Trojan <leo.troyan.melo@gmail.com>
2026-01-26 02:04:54 -0300

    o usuario é redirecionado com o cpf após finalizar o pedido

--- self-checkout/src/app/[slug]/menu/actions/create-order.ts
+++ self-checkout/src/app/[slug]/menu/actions/create-order.ts
@@ -58,5 +58,5 @@ export const createOrder = async (input: createOrderInput) => {
             restaurantId: restaurant.id,
         }
     })
-    redirect(`/${input.slug}/orders`)
+    redirect(`/${input.slug}/orders?cpf=${removeCpfPunctuation(input.customerCPF)}`)
 }
