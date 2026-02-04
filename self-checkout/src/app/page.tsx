638812bee3d1ddad14d9c3f7377c03242da63500
Leonardo Trojan <leo.troyan.melo@gmail.com>
2026-02-04 13:28:16 -0300

    fix: redirect user to restaurant

--- self-checkout/src/app/page.tsx
+++ self-checkout/src/app/page.tsx
@@ -1,7 +1,5 @@
-const HomePage = () => {
-  return ( 
-    <h1>Hello World</h1>
-   );
+import { redirect } from "next/navigation";
+
+export default function Home() {
+  redirect("/fsw-donalds")
 }
- 
-export default HomePage;
